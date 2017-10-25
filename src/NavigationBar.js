095ac395f6ac332557200931d15f971247bbb050
Mathias Desloges <freakdev@gmail.com>
2017-10-25 22:21:58 +0200

    implement navbar customization

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -2,20 +2,24 @@ import React from 'react'
 
 import { Link } from 'react-router-dom';
 
-export default ({ pages }) => {
+export default ({ pages, ...props }) => {
     return (
-        <nav>
-            <ul>{
-                pages.map((page, k) => {
-                    return (
-                        <li key={k}>
-                            <Link to={ page.path }>
-                                { page.name }
-                            </Link>
-                        </li>    
-                    )
-                })
-            }</ul>
-        </nav>
+        <nav>{
+            props.render ? 
+                props.render(pages) :
+                (
+                <ul>{
+                    pages.map((page, k) => {
+                        return (
+                            <li key={k}>
+                                <Link to={ page.path }>{
+                                    props.link ? props.link(page) : page.name
+                                }</Link>
+                            </li>    
+                        )
+                    })
+                }</ul>
+                )
+        }</nav>
     )
 }
