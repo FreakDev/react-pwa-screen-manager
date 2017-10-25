7b4ec68184b12154dc4314c33b42685adbeb714e
Mathias Desloges <freakdev@gmail.com>
2017-10-26 00:45:15 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ import PageManager from './PageManager';
 import Page from './Page';
 import NavigationBar from './NavigationBar';
 
-import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(
-    <div>
-        <PageManager>
-            <Page style={{ background: 'red' }} name="home" exact path="/" />
-            <Page style={{ background: 'green' }} name="page 2" exact path="/two" />
-            <Page style={{ background: 'blue' }} name="page 3" exact path="/three" />
-            <NavigationBar position="bottom" />
-        </PageManager>
-    </div>
-    , document.getElementById('root'));
-registerServiceWorker();
+<div>
+    <PageManager>
+        <Page style={{ background: 'red' }} name="home" exact path="/" />
+        <Page style={{ background: 'green' }} name="page 2" exact path="/two" />
+        <Page style={{ background: 'blue' }} name="page 3" exact path="/three" />
+        <NavigationBar position="bottom" />
+    </PageManager>
+</div>
+, document.getElementById('root'));
