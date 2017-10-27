da610fd8e96906733c9d8ff4abfec0e579ae354d
Mathias Desloges <freakdev@gmail.com>
2017-10-27 18:25:02 +0200

    fix import

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-export { default as ScreenManager } from '../lib'
+export { default as ScreenManager } from './ScreenManager'
 export { default as Screen } from './Screen'
 export { default as NavigationBar } from './NavigationBar'
 export { default as ForbiddenScreen } from './ForbiddenScreen'
