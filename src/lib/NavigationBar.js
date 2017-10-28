bd691b7953c642248386969e48ca87bca393adf6
Mathias Desloges <freakdev@gmail.com>
2017-10-28 20:58:40 +0200

    implement the active status for navigation bar button

--- src/lib/NavigationBar.js
+++ src/lib/NavigationBar.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom'
 
 import './NavigationBar.css'
 
-export default ({ pages, position, ...props }) => {
+export default ({ pages, position, location, ...props }) => {
     return (
         <nav className={'navbar ' + position}>{
             props.render ? 
-                props.render(pages) :
+                props.render(pages, location) :
                 (
                 <ul>{
                     Array.isArray(pages) && pages.map((page, k) => {
                         return (
-                            <li key={k}>
+                            <li key={k}  className={ location.pathname === page.path ? 'active' : '' }>
                                 <Link to={ page.path }>{
-                                    props.link ? props.link(page) : page.name
+                                    props.link ? props.link(page, location.pathname === page.path) : page.name
                                 }</Link>
                             </li>    
                         )
